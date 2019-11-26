eff7bd63513ac05b79354711722a309b0fdb8f6b
Mikhaela Cueto <mikhaelal.cueto@pccw.com>
2019-11-26 17:02:35 +0800

    initial commit

---
+++ application/views/home.php
@@ -0,0 +1,81 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+test home
+<!-- <div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-container">
+                    <div class="card-body">
+                        <div class="card-content">
+                            Tokyo
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-container">
+                    <div class="card-body">
+                        <div class="card-content">
+                            Yokohama
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-container">
+                    <div class="card-body">
+                        <div class="card-content">
+                            Kyoto
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-container">
+                    <div class="card-body">
+                        <div class="card-content">
+                            Osaka
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-container">
+                    <div class="card-body">
+                        <div class="card-content">
+                            Sapporo
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-container">
+                    <div class="card-body">
+                        <div class="card-content">
+                            Nagoya
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
---
+++ application/views/layouts/default_layout.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf_8">
+    <title> Tour to Japan </title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(''); ?>/public/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/public/css/style.scss">
+    <link rel="icon" type="image/x-icon" href="<?php echo base_url() ?>/public/img/tour-to-japan-logo.png" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/public/common/fontawesome-free/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <!-- page specific plugin styles -->
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div id="top-background" class="w-100 m-b-15 p-relative">
+                <div class="w-100 p-relative">
+                    <div class="bg-img-mask w100 p-absolute"></div>
+                </div>
+
+                <h1 id="page-title" class="toptext p-absolute w100 std-align main red" style="z-index:20;">
+                    Japan <br>
+                    Endless Discovery
+                </h1>
+            </div>
+        </div>
+
+        <div class="main">
+            <div class="content w100 std-algn main float">
+                <div class="container">
+                    <div class="row flex-grid-thirds">
+                        <div class="col col-flex">
+                            <div class="card card-flex">
+                                <div class="card-container">
+                                    <div class="card-body">
+                                        <div class="card-content">
+                                            <div class="city-details">
+                                                <div class="card-title">
+                                                    <span>
+                                                        <i class="fas fa-map-marker-alt"></i> Tokyo
+                                                    </span>
+                                                </div>
+
+                                                <div class="city-bg">
+                                                    <img src="<?php echo base_url() ?>/public/img/tokyo/tokyo-card-bg-2.jpg" alt="tokyo-cherry-blossom">
+                                                </div>
+
+                                                <div class="city-info">
+                                                    <span>
+                                                        A paradise-like oasis of green in the heart of busy Tokyo, Ueno Park is the city's largest green space and one of its most popular tourist attractions.
+                                                        Address: 9-83, Ueno Park, Tokyo, Kanto 110-8711
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-flex col">
+                            <div class="card card-flex">
+                                <div class="card-container">
+                                    <div class="card-body">
+                                        <div class="card-content">
+                                            Yokohama
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col col-flex">
+                            <div class="card card-flex">
+                                <div class="card-container">
+                                    <div class="card-body">
+                                        <div class="card-content">
+                                            Kyoto
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row flex-grid-thirds">
+                        <div class="col col-flex">
+                            <div class="card card-flex">
+                                <div class="card-container">
+                                    <div class="card-body">
+                                        <div class="card-content">
+                                            Osaka
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col col-flex">
+                            <div class="card card-flex">
+                                <div class="card-container">
+                                    <div class="card-body">
+                                        <div class="card-content">
+                                            Sapporo
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col col-flex">
+                            <div class="card card-flex">
+                                <div class="card-container">
+                                    <div class="card-body">
+                                        <div class="card-content">
+                                            Nagoya
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
